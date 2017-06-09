605af195989b09e5bb7a3ba8614cda2fbc1d26b7
tayvionne <tayvionne@users.noreply.github.com>
2017-06-09 16:12:51 -0400

    Add files via upload
    Python programming to calculate distance traveled by multiplying speed by time.

---
+++ M2HW1_DistanceTraveled_CAREY.py
@@ -0,0 +1,44 @@
+# Calculate Distance Traveled
+# June 9, 2017
+# CTI-110 M2HW1 - Distance Traveled
+# TayVionne Carey
+#
+
+
+#Assign a value to the speed variable
+speed = 70
+
+#Assign values for time.
+time = 6
+
+#Calculate distance by multiplying speed and time
+distance = speed * time
+
+#Display the distance
+print('The distance is', distance)
+
+
+#Assign a value to the speed variable
+speed = 70
+
+#Assign values for time.
+time = 10
+
+#Calculate distance by multiplying speed and time
+distance = speed * time
+
+#Display the distance
+print('The distance is', distance)
+
+
+#Assign a value to the speed variable
+speed = 70
+
+#Assign values for time.
+time = 15
+
+#Calculate distance by multiplying speed and time
+distance = speed * time
+
+#Display the distance
+print('The distance is', distance)
